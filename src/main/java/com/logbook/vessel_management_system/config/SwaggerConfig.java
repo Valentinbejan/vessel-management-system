fe88f96efeb18017730c6d4ceac7472911b63f6e
Valentinbejan <valentinbejan@yahoo.com>
2025-05-15 22:51:58 +0300

    Feat: Add Swagger API documentation using Springdoc-2

--- src/main/java/com/logbook/vessel_management_system/config/SwaggerConfig.java
+++ src/main/java/com/logbook/vessel_management_system/config/SwaggerConfig.java
@@ -2,9 +2,7 @@
 package com.logbook.vessel_management_system.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
